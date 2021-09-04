411f0144fc632d62f303743c9158ffc387ae678c
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-09-04 12:06:04 -0500

    Added "template" functionality to HTML, now heroes can be selected from the list

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import * as template from './templates/templates.js'
 import * as func from './modules/functions.js'
 
 const searchHeroBar = document.getElementById('searchHeroBar')
@@ -9,35 +8,45 @@ const charName = document.getElementById('charName')
 const charDescription = document.getElementById('charDescription')
 const characterGalery = document.getElementById('characterGalery')
 const characterSearchResults = document.getElementById('characterSearchResults')
+const templateSearchResults = document.getElementById('templateSearchResults')
+const templateCharacterRender = document.getElementById('templateCharacterRender')
 
-let PRIVATEDATA
-let PRIVATEDATA_isLoaded = false
+let ACCESSDATA
+let ACCESSDATA_isLoaded = false
 
-const getPrivateData = async() => {
+const getAccessData = async() => {
     await fetch("./js/json/APIaccessData.json")
         .then(res => res.json())
         .then(data => {
-            PRIVATEDATA = data
-            PRIVATEDATA_isLoaded = true
+            ACCESSDATA = data
+            ACCESSDATA_isLoaded = true
         })
 }
-getPrivateData()
+getAccessData()
 
 const marvel = {
     renderCharacter: (name) => {
-        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${PRIVATEDATA.tsAccess}&name=${name}&apikey=${PRIVATEDATA.publicKey}&${PRIVATEDATA.md5HashAccess}`;
-        //let contentHTML = '';
+        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&name=${name}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`;
         fetch(URLAPI)
             .then (res => res.json())
             .then((json) => {
+                const heroSelected = json.data.results[0]
                 //Code to create page of character
-                characterSearchResults.innerHTML = ""
-                console.log("RES JSON: ", json)
-                characterGalery.innerHTML = template.createHeroDisplay(json.data.results[0].name, json.data.results[0].description, `${json.data.results[0].thumbnail.path}.${json.data.results[0].thumbnail.extension}`)
+                {
+                    characterSearchResults.innerHTML = ""
+                    characterGalery.innerHTML = ""
+                    console.log("RES JSON: ", json)
+                    let tempNode = templateCharacterRender.content
+                    let htmlNode = document.importNode(tempNode,true)
+                    htmlNode.querySelector('.character_name').textContent = heroSelected.name
+                    htmlNode.querySelector('.character_thumbnail').setAttribute("src", `${heroSelected.thumbnail.path}.${heroSelected.thumbnail.extension}`)
+                    htmlNode.querySelector('.character_description').textContent = heroSelected.description
+                    characterGalery.appendChild(htmlNode)
+                }
             })
     },
     createListOfCharacters: (startsWith) => {
-        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${PRIVATEDATA.tsAccess}&nameStartsWith=${startsWith}&apikey=${PRIVATEDATA.publicKey}&${PRIVATEDATA.md5HashAccess}`;
+        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&nameStartsWith=${startsWith}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`;
         fetch(URLAPI)
             .then (res => res.json())
             .then (json => {
@@ -45,15 +54,19 @@ const marvel = {
                 //Code to create list of results
                 {
                     characterSearchResults.innerHTML = ""
+                    characterGalery.innerHTML = ""
                     for (let index = 0; index < json.data.results.length; index++) {
                         const heroFound = json.data.results[index];
-                        characterSearchResults.innerHTML = characterSearchResults.innerHTML + template.characterSearchResult(index, heroFound.name, `${heroFound.thumbnail.path}.${heroFound.thumbnail.extension}`)
-                        // func.$(`resultID${index}`).addEventListener("click", () => {
-                        //     alert(`Click on ID ${index}`)
-                        //     console.log('Hola')
-                        // })
+                        let tempNode = templateSearchResults.content
+                        let htmlNode = document.importNode(tempNode,true)
+                        htmlNode.querySelector('.character_search_result__container__name').textContent = heroFound.name
+                        htmlNode.querySelector('.character_search_result__picture').setAttribute("src", `${heroFound.thumbnail.path}.${heroFound.thumbnail.extension}`)
+                        htmlNode.querySelector('.character_search_result__container').setAttribute("id", `resultID${index}`)
+                        characterSearchResults.appendChild(htmlNode)
+                        func.$(`resultID${index}`).addEventListener("click" , () => {
+                            marvel.renderCharacter(heroFound.name)
+                        })
                     }
-                    
                 }
             })
     }
