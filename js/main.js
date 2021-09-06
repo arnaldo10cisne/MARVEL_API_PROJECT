395f36093fc89fa5c6215a708b8a38bf0e5e96a5
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-09-06 00:46:52 -0500

    Comics are now selectable

--- js/main.js
+++ js/main.js
@@ -7,29 +7,55 @@ const charThumbnail = document.getElementById('charThumbnail')
 const charName = document.getElementById('charName')
 const charDescription = document.getElementById('charDescription')
 const characterGalery = document.getElementById('characterGalery')
+const comicGalery = document.getElementById('comicGalery')
 const characterSearchResults = document.getElementById('characterSearchResults')
 const templateSearchResults = document.getElementById('templateSearchResults')
 const templateCharacterRender = document.getElementById('templateCharacterRender')
-
-
-
+const templateComicRender = document.getElementById('templateComicRender')
 
 let ACCESSDATA
-let ACCESSDATA_isLoaded = false
 
-const getAccessData = async() => {
-    await fetch("./js/json/APIaccessData.json")
-        .then(res => res.json())
-        .then(data => {
-            ACCESSDATA = data
-            ACCESSDATA_isLoaded = true
-        })
+const getAccessData = async () => {
+    const response = await fetch("./js/json/APIaccessData.json")
+    const data = await response.json()
+    ACCESSDATA = data
 }
-getAccessData()
+
+const cleanGuidebook = () => {
+    characterSearchResults.innerHTML = ""
+    characterGalery.innerHTML = ""
+    comicGalery.innerHTML = ""
+}
 
 const marvel = {
+    createListOfCharacters: async (startsWith) => {
+        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&nameStartsWith=${startsWith}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`;
+        const response = await fetch(URLAPI)
+        const json = await response.json()
+        
+        // Code to create list of possible choices
+        {
+            cleanGuidebook()
+            if (json.data.results.length == 1) {
+                marvel.renderCharacter(json.data.results[0].name)
+            } else {
+                for (let index = 0; index < json.data.results.length; index++) {
+                    const heroFound = json.data.results[index];
+                    let tempNode = templateSearchResults.content
+                    let htmlNode = document.importNode(tempNode,true)
+                    htmlNode.querySelector('.character_search_result__container__name').textContent = heroFound.name
+                    htmlNode.querySelector('.character_search_result__picture').setAttribute("src", `${heroFound.thumbnail.path}.${heroFound.thumbnail.extension}`)
+                    htmlNode.querySelector('.character_search_result__container').setAttribute("id", `resultID${index}`)
+                    characterSearchResults.appendChild(htmlNode)
+                    func.$(`resultID${index}`).addEventListener("click" , () => {
+                        marvel.renderCharacter(heroFound.name)
+                    })
+                }
+            }
+        }
+    },
     renderCharacter: async (name) => {
-        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&name=${name}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`;
+        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&name=${name}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`
         const response = await fetch(URLAPI)
         const json = await response.json()    
         const heroSelected = await json.data.results[0]
@@ -38,10 +64,9 @@ const marvel = {
         let seriesThumbnailsURI = []
         let storiesURI = []
         
-        //Code to create page of character
+        //Code to create render and description of character
         {
-            characterSearchResults.innerHTML = ""
-            characterGalery.innerHTML = ""
+            cleanGuidebook()
             console.log("HERO SELECTED: ", json)
             let tempNode = templateCharacterRender.content
             let htmlNode = document.importNode(tempNode,true)
@@ -63,6 +88,15 @@ const marvel = {
                 htmlNode.querySelector('.comic_container__thumbnail').setAttribute("id", `comicPicture${index}`)
                 comicThumbnailsURI.push(comicFound.resourceURI)
                 comicsRail.appendChild(htmlNode)
+                func.$(`comicPicture${index}`).addEventListener("click" , async () => {
+                    let comicSelected = `${comicThumbnailsURI[index]}?${ACCESSDATA.tsAccess}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`
+                    if (comicSelected.startsWith('http://')) {
+                        comicSelected = `https${comicSelected.slice(4)}`
+                    }
+                    const response = await fetch(comicSelected)
+                    const json = await response.json()
+                    marvel.renderComic(json.data.results[0].id)
+                })
             }
         }
 
@@ -102,7 +136,6 @@ const marvel = {
                 if (comicSelected.startsWith('http://')) {
                     comicSelected = `https${comicSelected.slice(4)}`
                 }
-                console.log(comicSelected)
                 const response = await fetch(comicSelected)
                 const json = await response.json()
                 func.$(`comicPicture${index}`).src = `${json.data.results[0].thumbnail.path}.${json.data.results[0].thumbnail.extension}`
@@ -116,41 +149,60 @@ const marvel = {
                 if (seriesSelected.startsWith('http://')) {
                     seriesSelected = `https${seriesSelected.slice(4)}`
                 }
-                console.log(seriesSelected)
                 const response = await fetch(seriesSelected)
                 const json = await response.json()
                 func.$(`seriesPicture${index}`).src = `${json.data.results[0].thumbnail.path}.${json.data.results[0].thumbnail.extension}`
             }
         }
     },
-    createListOfCharacters: (startsWith) => {
-        const URLAPI = `https://gateway.marvel.com:443/v1/public/characters?${ACCESSDATA.tsAccess}&nameStartsWith=${startsWith}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`;
-        fetch(URLAPI)
-            .then (res => res.json())
-            .then (json => {
-                console.log(json)
-                //Code to create list of results
-                {
-                    characterSearchResults.innerHTML = ""
-                    characterGalery.innerHTML = ""
-                    for (let index = 0; index < json.data.results.length; index++) {
-                        const heroFound = json.data.results[index];
-                        let tempNode = templateSearchResults.content
-                        let htmlNode = document.importNode(tempNode,true)
-                        htmlNode.querySelector('.character_search_result__container__name').textContent = heroFound.name
-                        htmlNode.querySelector('.character_search_result__picture').setAttribute("src", `${heroFound.thumbnail.path}.${heroFound.thumbnail.extension}`)
-                        htmlNode.querySelector('.character_search_result__container').setAttribute("id", `resultID${index}`)
-                        characterSearchResults.appendChild(htmlNode)
-                        func.$(`resultID${index}`).addEventListener("click" , () => {
-                            marvel.renderCharacter(heroFound.name)
-                        })
-                    }
-                }
-            })
-    }
+    renderComic: async (id) => {
+
+        const URLAPI = `https://gateway.marvel.com:443/v1/public/comics/${id}?${ACCESSDATA.tsAccess}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`
+        const response = await fetch(URLAPI)
+        const json = await response.json()    
+        const comicSelected = await json.data.results[0]
+
+        // Code to render Comic information
+        {
+            cleanGuidebook()
+            console.log('COMIC SELECTED: ', json)
+            let tempNode = templateComicRender.content
+            let htmlNode = document.importNode(tempNode,true)
+            htmlNode.querySelector('.comic_name').textContent = comicSelected.title
+            htmlNode.querySelector('.comic_thumbnail').setAttribute("src", `${comicSelected.thumbnail.path}.${comicSelected.thumbnail.extension}`)
+            htmlNode.querySelector('.comic_description').textContent = comicSelected.description
+            comicGalery.appendChild(htmlNode)
+        }
+
+        // Code to create list of characters
+        // {
+        //     const comicGalery_charactersRailTemplate = document.getElementById('comicGalery_charactersRailTemplate')
+        //     const comicsRail = document.getElementById('comicsRail')
+        //     for (let index = 0; index < heroSelected.comics.items.length; index++) {
+        //         const comicFound = heroSelected.comics.items[index];
+        //         let tempNode = comicRailTemplate.content
+        //         let htmlNode = document.importNode(tempNode,true)
+        //         htmlNode.querySelector('.comic_container__name').textContent = (comicFound.name.length > 30) ? `${comicFound.name.slice(0,30)} ...` : `${comicFound.name}`
+        //         htmlNode.querySelector('.comic_container__thumbnail').setAttribute("id", `comicPicture${index}`)
+        //         comicThumbnailsURI.push(comicFound.resourceURI)
+        //         comicsRail.appendChild(htmlNode)
+        //         func.$(`comicPicture${index}`).addEventListener("click" , async () => {
+        //             let comicSelected = `${comicThumbnailsURI[index]}?${ACCESSDATA.tsAccess}&apikey=${ACCESSDATA.publicKey}&${ACCESSDATA.md5HashAccess}`
+        //             if (comicSelected.startsWith('http://')) {
+        //                 comicSelected = `https${comicSelected.slice(4)}`
+        //             }
+        //             const response = await fetch(comicSelected)
+        //             const json = await response.json()
+        //             marvel.renderComic(json.data.results[0].id)
+        //         })
+        //     }
+        // }
+    },
 }
 
 searchButton.addEventListener("click", ()=>{
     let hero = searchHeroBar.value
     marvel.createListOfCharacters(hero)
 })
+
+getAccessData()
